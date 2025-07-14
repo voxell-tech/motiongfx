bf11f233678d06e469a2193da97f5023b3367c64
Nixon <43715558+nixon-voxell@users.noreply.github.com>
2025-07-14 23:13:24 +0800

    Add custom easing example

--- examples/custom_ease.rs
+++ examples/custom_ease.rs
@@ -1,3 +1,4 @@
+use bevy::color::palettes;
 use bevy::prelude::*;
 use motiongfx::prelude::*;
 
@@ -12,25 +13,52 @@ fn main() {
         .run();
 }
 
+fn spawn_timeline(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Spawn cube.
+    let cube = commands
+        .spawn((
+            Mesh3d(meshes.add(Cuboid::default())),
+            MeshMaterial3d(materials.add(StandardMaterial {
+                base_color: palettes::tailwind::LIME_200.into(),
+                ..default()
+            })),
+            Transform::from_xyz(-5.0, 0.0, 0.0),
+        ))
+        .id();
+
+    // Generate sequence.
+    let sequence = commands
+        .entity(cube)
+        .act(field!(<Transform>::translation::x), |x| x + 10.0)
+        // A custom 10 step easing.
+        .with_ease(|t| ((t * 10.0) as u32) as f32 / 10.0)
+        .play(1.0);
+
+    commands
+        .create_timeline(sequence)
+        .insert(TimelinePlayback::Forward);
+}
+
 fn setup(mut commands: Commands) {
     commands.spawn((
         Camera {
-            hdr: true,
+            clear_color: Color::BLACK.into(),
             ..default()
         },
         Camera3d::default(),
         Transform::from_xyz(0.0, 0.0, 15.0),
     ));
 
-    // Directional light
     commands.spawn((
         DirectionalLight::default(),
         Transform::from_xyz(3.0, 10.0, 5.0)
             .looking_at(Vec3::ZERO, Vec3::Y),
     ));
 }
-
-fn spawn_timeline(mut commands: Commands) {}
 
 fn timeline_movement(
     mut q_timelines: Query<(&Timeline, &mut TimelinePlayback)>,
