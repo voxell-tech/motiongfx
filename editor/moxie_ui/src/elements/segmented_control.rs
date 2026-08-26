71887dbd522f414b8ac5b9d07b9bb3d2c97c6341
Nixon <43715558+nixonyh@users.noreply.github.com>
2026-08-26 14:10:02 +0800

    Fix doc

--- editor/moxie_ui/src/elements/segmented_control.rs
+++ editor/moxie_ui/src/elements/segmented_control.rs
@@ -1,6 +1,8 @@
 //! A row of mutually exclusive options, as a composer rather than an
 //! element - it builds a subtree of [`ButtonElem`]s, none of it kept
 //! around to be patched later.
+//!
+//! [`ButtonElem`]: crate::elements::button::ButtonElem
 
 use bevy::prelude::*;
 use bevy::ui_widgets::Activate;
