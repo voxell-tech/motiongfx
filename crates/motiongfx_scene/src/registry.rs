70a7ccd26593fce310ee17774a2d45a9e59faee1
Nixon <43715558+nixonyh@users.noreply.github.com>
2026-09-06 22:29:20 +0800

    Use `motiongfx::prelude::*`

--- crates/motiongfx_scene/src/registry.rs
+++ crates/motiongfx_scene/src/registry.rs
@@ -7,16 +7,10 @@ use alloc::boxed::Box;
 
 use educe::Educe;
 use hashbrown::HashMap;
-use typarena::type_table::TypeTable;
-
-use motiongfx::ThreadSafe;
-use motiongfx::action::{Action, InterpFn};
 use motiongfx::field_path::field::UntypedField;
-use motiongfx::motiongfx_interp::ease::EaseFn;
 use motiongfx::prelude::*;
-use motiongfx::registry::Registry;
 use motiongfx::subject::SubjectId;
-use motiongfx::world::SubjectSource;
+use typarena::type_table::TypeTable;
 
 use crate::backend::{IntoSubjectId, SceneBackend};
 use crate::block::ActionCmd;
