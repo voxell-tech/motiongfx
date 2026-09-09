3b050cf01227d004f5d7ce89c736628587495977
Nixon <43715558+nixonyh@users.noreply.github.com>
2026-09-09 09:52:58 +0800

    Guard registry within scene cfg feature

--- crates/bevy_motiongfx/src/lib.rs
+++ crates/bevy_motiongfx/src/lib.rs
@@ -8,8 +8,6 @@ use bevy_ecs::prelude::*;
 
 use crate::controller::ControllerPlugin;
 use crate::manager::MotionGfxManagerPlugin;
-#[cfg(feature = "scene")]
-use crate::scene::MotionGfxScenePlugin;
 
 pub mod controller;
 pub mod interpolation;
@@ -50,12 +48,13 @@ impl Plugin for BevyMotionGfxPlugin {
                 .chain(),
         );
 
-        scene::value_pool::register_scene_values(app);
-
         app.add_plugins((MotionGfxManagerPlugin, ControllerPlugin));
 
         #[cfg(feature = "scene")]
-        app.add_plugins(MotionGfxScenePlugin);
+        {
+            app.add_plugins(scene::MotionGfxScenePlugin);
+            scene::value_pool::register_scene_values(app);
+        }
     }
 }
 
